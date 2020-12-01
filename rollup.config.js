fc8f9adb69de538883b080be6b98a1bc2180d2b8
Kapelianovych Yevhen <kapelianovych.y.v@gmail.com>
2020-12-01 17:05:22 +0200

    Simplify rollup config.

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,15 @@
-import fs from 'fs';
-import path from 'path';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 
 const SOURCE_DIR_NAME = 'src';
 
 export default {
-  input: fs
-    .readdirSync(path.resolve(process.cwd(), SOURCE_DIR_NAME), {
-      withFileTypes: true,
-    })
-    .filter((dirent) => !dirent.isDirectory() && dirent.name !== 'types.ts')
-    .map((dirent) => `${SOURCE_DIR_NAME}/${dirent.name}`),
+  input: `${SOURCE_DIR_NAME}/index.ts`,
   output: {
     dir: 'build',
     format: 'es',
     preserveModules: true,
+    preserveModulesRoot: SOURCE_DIR_NAME,
   },
   plugins: [typescript(), terser()],
 };
